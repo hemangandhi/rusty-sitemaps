2d75bc9ff31e04824bd5c13a07119c9b42c95384
Hari Amoor <amoor.hari@gmail.com>
2020-04-08 20:58:06 -0400

    finished api - still need to test

--- lib-sitemap/src/lib.rs
+++ lib-sitemap/src/lib.rs
@@ -1,34 +1,64 @@
+extern crate async_std;
+extern crate futures;
 extern crate log;
+extern crate reqwest;
+extern crate select;
 extern crate serde_json;
 
 use log::{error, info};
 
 use std::collections::HashMap;
-use std::io;
+use std::io::Write;
 use std::ops::Drop;
 
-struct SitemapGenerator<T: io::Write + Copy> {
+use select::document::Document;
+use select::predicate::Name;
+
+struct SitemapGenerator<T: Write + Copy> {
     table: HashMap<String, Vec<String>>,
-    output_src: T,
+    sink: T,
     root_link: String,
 }
 
-impl<T: io::Write + Copy> Drop for SitemapGenerator<T> {
+impl<T: Write + Copy> Drop for SitemapGenerator<T> {
     fn drop(&mut self) {
-        match serde_json::to_writer_pretty(self.output_src, &self.table) {
+        match serde_json::to_writer_pretty(self.sink, &self.table) {
             Ok(_) => info!("Successfully written to input source"),
             Err(e) => error!("{}", e),
         }
     }
 }
 
-impl<T: io::Write + Copy> SitemapGenerator<T> {
-    fn new(link: &str, output_src: T) -> SitemapGenerator<T> {
+impl<T: Write + Copy> SitemapGenerator<T> {
+    fn new(link: &str, sink: T) -> SitemapGenerator<T> {
         SitemapGenerator {
             table: HashMap::new(),
-            output_src: output_src,
+            sink: sink,
             root_link: String::from(link),
         }
+    }
+
+    fn parse(&mut self, links: &Vec<String>) -> Result<(), Box<dyn std::error::Error>> {
+        for link in links.iter() {
+            if !self.table.contains_key(link) {
+                let body: &str = &reqwest::blocking::get(link)?.text()?;
+
+                let children: Vec<_> = Document::from(body)
+                    .find(Name("a"))
+                    .filter_map(|n| n.attr("href"))
+                    .map(String::from)
+                    .collect();
+
+                self.table.insert(String::from(link), children.clone());
+                self.parse(&children)?;
+            }
+        }
+
+        Ok(())
+    }
+
+    fn start(&mut self) -> Result<(), Box<dyn std::error::Error>> {
+        self.parse(&vec![self.root_link.clone()])
     }
 }
 
